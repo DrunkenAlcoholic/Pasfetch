4518ad950eb3f689b4cb951ade659bde19881bc3
Drunken Alcoholic <drunkenalcoholic@protonmail.com>
2023-02-08 17:01:26 +0000

    Update cleanup.sh to reduce compiled file size for x86_64

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 ## compile with fpc + flags
-fpc -O3 -CpCOREAVX2 -OpCOREAVX2 -CfAVX2 -Xs pasfetch.pas
+## fpc -O3 -CpCOREAVX2 -OpCOREAVX2 -CfAVX2 -Xs pasfetch.pas
+
+## An update to reduce compiled file size
+fpc pasfetch.pas -Px86_64 -Mobjfpc -CX -O3 -XXs -B -v
 
 
 ## Remove object & discriptive files
